f5fe6e4b5eecdeebcc501b5be0ebf66cc987e009
cawizca <kavishkam1@gmail.com>
2021-09-25 16:29:54 +0530

    Contact us, Home, Service and splash screens completed.

--- app/src/main/java/com/futuregen/bocandroid/HomeActivity.java
+++ app/src/main/java/com/futuregen/bocandroid/HomeActivity.java
@@ -6,25 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-import com.futuregen.bocandroid.IT19207100.BudgetCalculator;
+import com.futuregen.bocandroid.Contact.ContactActivity;
+import com.futuregen.bocandroid.Services.ServiceMenu;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageButton service_btn;
+    private ImageButton contact_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button budgetCalculator = findViewById(R.id.budget);
+        service_btn = (ImageButton) findViewById(R.id.btn_service);
 
-        budgetCalculator.setOnClickListener(new View.OnClickListener() {
+        service_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent in =new Intent(HomeActivity.this, BudgetCalculator.class);
-                startActivity(in);
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(HomeActivity.this, ServiceMenu.class);
+                startActivity(serviceIntent);
             }
         });
 
+        contact_btn = (ImageButton) findViewById(R.id.button_contact);
+
+        contact_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contsctIntent = new Intent(HomeActivity.this, ContactActivity.class);
+                startActivity(contsctIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/futuregen/bocandroid/MainPack/HeaderActivity.java
@@ -0,0 +1,16 @@
+package com.futuregen.bocandroid.MainPack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.futuregen.bocandroid.R;
+
+public class HeaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header);
+    }
+}
