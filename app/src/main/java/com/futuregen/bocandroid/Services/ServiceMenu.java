91cd5ab226281a9ea480ce3883a4568f5458e859
shanuka-axceera <shanuka.prabodha@axceera.com>
2021-09-25 22:04:14 +0530

    budget calculator UI completed

--- app/src/main/java/com/futuregen/bocandroid/Services/ServiceMenu.java
+++ app/src/main/java/com/futuregen/bocandroid/Services/ServiceMenu.java
@@ -2,8 +2,15 @@ package com.futuregen.bocandroid.Services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.futuregen.bocandroid.HomeActivity;
+import com.futuregen.bocandroid.IT19207100.BudgetCalculator;
 import com.futuregen.bocandroid.R;
 
 public class ServiceMenu extends AppCompatActivity {
@@ -12,5 +19,14 @@ public class ServiceMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_menu);
+        ImageView budget = findViewById(R.id.budget);
+
+        budget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in =new Intent(ServiceMenu.this, BudgetCalculator.class);
+                startActivity(in);
+            }
+        });
     }
 }
