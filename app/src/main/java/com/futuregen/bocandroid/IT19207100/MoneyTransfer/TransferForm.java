2ababfe41ec226701879165902757ac97f6942b7
shanuka-prabodha <shanukaprabodha1262@gmail.com>
2021-09-27 08:24:42 +0530

    enable/disable function is created

--- app/src/main/java/com/futuregen/bocandroid/IT19207100/MoneyTransfer/TransferForm.java
+++ app/src/main/java/com/futuregen/bocandroid/IT19207100/MoneyTransfer/TransferForm.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.futuregen.bocandroid.R;
 
@@ -17,6 +21,16 @@ public class TransferForm extends AppCompatActivity {
         setContentView(R.layout.activity_transfer_form);
 
         Button paynow = findViewById(R.id.paynow);
+        Switch switch1 = findViewById(R.id.switch1);
+        EditText acno = findViewById(R.id.acno);
+        EditText acname = findViewById(R.id.acname);
+        Spinner spinnerTransfer = findViewById(R.id.spinnerTransfer);
+
+
+        acno.setEnabled(false);
+        acname.setEnabled(false);
+
+
 
         paynow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +39,23 @@ public class TransferForm extends AppCompatActivity {
                 startActivity(serviceIntent);
             }
         });
+
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked==true){
+                    acno.setEnabled(true);
+                    acname.setEnabled(true);
+                    spinnerTransfer.setEnabled(false);
+                }
+                else{
+                    acno.setEnabled(false);
+                    acname.setEnabled(false);
+                    spinnerTransfer.setEnabled(true);
+                }
+            }
+        });
+
     }
 }
