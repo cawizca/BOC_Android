688a2903dd76e1fa7115260b07fe35afb49ba0df
udaramadhuranga <udaramadhu128@gmail.com>
2021-10-10 00:15:25 +0530

    profile added linked to dashboard

--- app/src/main/java/com/futuregen/bocandroid/IT19209012/Dashboard.java
+++ app/src/main/java/com/futuregen/bocandroid/IT19209012/Dashboard.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.futuregen.bocandroid.IT19207100.MoneyTransfer.TransferForm;
+import com.futuregen.bocandroid.IT19210520.Profile;
 import com.futuregen.bocandroid.R;
 
 public class Dashboard extends AppCompatActivity {
@@ -16,7 +18,7 @@ public class Dashboard extends AppCompatActivity {
 
 
 
-
+ImageView profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,16 @@ public class Dashboard extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Dashboard.this, TransferForm.class);
                 startActivity(intent);
+            }
+        });
+
+        profile = findViewById(R.id.imgProfile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Dashboard.this, Profile.class);
+                startActivity(i);
             }
         });
 
