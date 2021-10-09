3a458c006c889346ee8631f8261e8d05c2dbe569
udaramadhuranga <udaramadhu128@gmail.com>
2021-10-09 22:55:23 +0530

    Validation added to sequrity,login,forgot,otp Activities

--- app/src/main/java/com/futuregen/bocandroid/IT19210520/LoginActivity.java
+++ app/src/main/java/com/futuregen/bocandroid/IT19210520/LoginActivity.java
@@ -5,19 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.futuregen.bocandroid.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView forgot;
+    Button unlock;
+    EditText username;
+    EditText Password;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         forgot =findViewById(R.id.txtForgot);
+        username = findViewById(R.id.loginUserNameId);
+        Password =findViewById(R.id.loginPasswordId);
+        login = findViewById(R.id.btnlogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String user = username.getText().toString();
+
+                final String p = Password.getText().toString();
+
+                validate(user,p);
+
+            }
+        });
 
         forgot.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +48,43 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        unlock = findViewById(R.id.btnUnlock);
+        unlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(LoginActivity.this,Profile.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+    public void validate(String username, String password) {
+
+        if ((username.equals("udaraMadhuranga")) && (password.equals("12345678"))) {
+
+            Intent i = new Intent(LoginActivity.this,Forgot.class);
+            startActivity(i);
+
+        }
+        else if(!username.equals("")&&!username.equals("udaraMadhuranga")){
+            Toast.makeText(LoginActivity.this, "Please enter correct username", Toast.LENGTH_SHORT).show();
+        }
+        else if(!password.equals("")&&!password.equals("12345678")){
+            Toast.makeText(LoginActivity.this, "Please enter correct password", Toast.LENGTH_SHORT).show();
+        }
+        else if(!username.equals("")&&!username.equals("udaraMadhuranga")&&!password.equals("")&&!password.equals("12345678")){
+            Toast.makeText(LoginActivity.this, "Please enter correct username and password", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(!username.equals("")&&username.equals("udaraMadhuranga")&&password.equals("")){
+            Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Toast.makeText(LoginActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
--- app/src/main/java/com/futuregen/bocandroid/IT19210520/OTP.java
+++ app/src/main/java/com/futuregen/bocandroid/IT19210520/OTP.java
@@ -6,25 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.futuregen.bocandroid.R;
 
 public class OTP extends AppCompatActivity {
 
     Button submit;
+    EditText o1;
+    EditText o2;
+    EditText o3;
+    EditText o4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
 
         submit = findViewById(R.id.bynOTPSub);
-
+        o1 = findViewById(R.id.otp1);
+        o2 = findViewById(R.id.otp1);
+        o3 = findViewById(R.id.otp3);
+        o4 = findViewById(R.id.otp4);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(OTP.this,SecurityQuesions.class);
-                startActivity(i);
+                final String ot1 = o1.getText().toString();
+                final String ot2 = o2.getText().toString();
+                final String ot3 =  o3.getText().toString();
+                final String ot4 = o4.getText().toString();
+                validate(ot1,ot2,ot3,ot4);
             }
         });
     }
+    public void validate(String o1,String o2,String o3,String o4) {
+
+        if ((o1.equals("3")) && (o2.equals("3")) && (o3.equals("3")) && (o3.equals("3"))) {
+
+            Intent i = new Intent(OTP.this,SecurityQuesions.class);
+            startActivity(i);
+
+        }     else if((!o1.equals("")&&!o1.equals("3")) || (!o2.equals("")&&!o2.equals("3") || (!o3.equals("")&&!o3.equals("3")) || (!o4.equals("")&&!o4.equals("3")))                                                      ){
+            Toast.makeText(OTP.this, "Please enter correct OTP Values", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
